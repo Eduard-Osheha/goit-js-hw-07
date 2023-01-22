c4a716cad38361f06e88fc416656412a5b05ab8c
Eduard-Osheha <eduardoshega07@gmail.com>
2023-01-22 16:37:30 +0100

    asdfc

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,43 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
 
-const createMurkup = {
-    
+const galleryContainer = document.querySelector(".gallery");
+const gallaryCards = createGalleryItemCard(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", gallaryCards);
+galleryContainer.addEventListener("click", onGalleryCardClick);
+
+function createGalleryItemCard(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" 
+      href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"          
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
 }
+
+function onGalleryCardClick (evt) {   
+    
+    const isLinkGalleryItem = evt.target.parentElement.classList.contains('gallery__link');
+    const urlLagreImage = evt.target.dataset.source;
+
+if (!isLinkGalleryItem) {
+  return;
+}
+
+   
+    console.log('Large image url: ', urlLagreImage);
+    return urlLagreImage;
+
+};
+
 
