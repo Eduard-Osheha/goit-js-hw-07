bf4800096e3ab80645f9134c5f3aa06b89dd68eb
Eduard-Osheha <eduardoshega07@gmail.com>
2023-01-22 13:16:48 +0100

    start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,9 @@
 import { galleryItems } from './gallery-items.js';
-
-
-
-
 // Change code below this line
 
 console.log(galleryItems);
 
-
+const createMurkup = {
+    
+}
 
--- js/myCode.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener(
-  "scroll", _.throttle(() => {
-    console.log("Scroll handler call every 300ms");
-  }, 300)
-);
